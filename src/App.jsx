f64c4188414cc34e30327b76e29234e01bb34e41
owen-zhou20 <owen.zhou20@gmail.com>
2023-03-11 18:41:00 +1100

    Route demo init

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
 import React, { Component } from 'react'
-import Search from './components/Search/'
-import List from './components/List/'
+import {NavLink,Route, Router} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 
 
 export default class App extends Component {
 
   render() {
     return (
-      <div className="container">
-        <Search/>
-        <List/>
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="page-header"><h2>React Router Demo</h2></div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+
+              {/* <a className="list-group-item active" href="./about.html">About</a>
+              <a className="list-group-item" href="./home.html">Home</a> */}
+                <NavLink activeClassName='sv' className="list-group-item" to="/about">About</NavLink>
+                <NavLink activeClassName='sv' className="list-group-item" to="/home">Home</NavLink>
+              
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                  <Route path="/about" component={About}/>
+                  <Route path="/home" component={Home}/>
+                
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
