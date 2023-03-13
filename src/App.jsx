3e7d6296cfd5af04c34d6226e8e611a7b1bc6027
owen-zhou20 <owen.zhou20@gmail.com>
2023-03-13 18:19:30 +1100

    Add a NavLink demo, Switch demo, Redirect demo and secondary route demo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import {NavLink,Route, Router} from 'react-router-dom'
+import {Route,Switch,Redirect} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import MyNavLink from './components/MyNavLink'
 
 
 export default class App extends Component {
@@ -20,17 +21,26 @@ export default class App extends Component {
 
               {/* <a className="list-group-item active" href="./about.html">About</a>
               <a className="list-group-item" href="./home.html">Home</a> */}
-                <NavLink activeClassName='sv' className="list-group-item" to="/about">About</NavLink>
-                <NavLink activeClassName='sv' className="list-group-item" to="/home">Home</NavLink>
-              
+                {/* <NavLink activeClassName='sv' className="list-group-item" to="/about">About</NavLink>
+                <NavLink activeClassName='sv' className="list-group-item" to="/home">Home</NavLink>  */}
+                
+                  <MyNavLink to="/about">About</MyNavLink>
+                  <MyNavLink to="/home">Home</MyNavLink>
+                
             </div>
           </div>
           <div className="col-xs-6">
             <div className="panel">
               <div className="panel-body">
-                  <Route path="/about" component={About}/>
-                  <Route path="/home" component={Home}/>
-                
+                <switch>
+                  <Switch>
+                    {/* <Route exact={true} path="/about" component={About}/>
+                    <Route exact={true} path="/home" component={Home}/> */}
+                    <Route path="/about" component={About}/>
+                    <Route path="/home" component={Home}/>
+                    <Redirect to="/about" />
+                  </Switch>
+                </switch>
               </div>
             </div>
           </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from 'react'
+import MyNavLink from '../../components/MyNavLink'
+import {Route,Switch,Redirect} from 'react-router-dom'
+import News from './News'
+import Message from './Message'
 
 export default class Home extends Component {
   render() {
     return (
-      <h3>Home...</h3>
+      <div>
+        <h3>Home...</h3>
+        <div>
+          <ul className="nav nav-tabs">
+            <li>
+              <MyNavLink to="/home/news">News</MyNavLink>
+            </li>
+            <li>
+              <MyNavLink to="/home/message">Message</MyNavLink>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/home/news" component={News}/>
+            <Route path="/home/message" component={Message}/>
+            <Redirect to="/home/news"/>
+          </Switch>
+        </div>
+      </div>
     )
   }
 }
